3ece448d45af8f24267a76065862c4a9200d94fe
Juwon Yi <juunewon@gmail.com>
2023-06-21 09:20:41 +0900

    초기세팅

---
+++ src/main/java/com/onemorething/monthlytest1/Monthlytest1Application.java
@@ -0,0 +1,13 @@
+package com.onemorething.monthlytest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Monthlytest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Monthlytest1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/onemorething/monthlytest1/Monthlytest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onemorething.monthlytest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Monthlytest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
